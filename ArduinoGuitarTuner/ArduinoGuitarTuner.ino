df5fd4ac6d3161a3624b6ee836d26d3d18aba977
eduardomy <eduardo.medina@cetys.edu.mx>
2016-12-01 18:28:57 -0800

    Code entirely rewriten using an example online. It can move servos right now.

--- ArduinoGuitarTuner/ArduinoGuitarTuner.ino
+++ ArduinoGuitarTuner/ArduinoGuitarTuner.ino
@@ -1,22 +1,52 @@
 #include <Servo.h>
+//#include <AudioFrequencyMeter.h>
  
 // 
 Servo servo;
 
 //pines
 short pinServo=37;
-short FREQREADER=4;
 short PBON=52, PBOFF=50, PBSTRING=53, PBSTART=51; // Declaracion de los readers de los botones PBON, PBOFF, PBSTRING, PBSTART, FREQ
 short LEDDONE=42, LEDON=43; // Declaracion de los outputs de los leds que vamos a encender LEDDONE, LEDON
 short LEDS[6]={48, 44, 46, 49, 47, 45};//E, A, D, G, B,  E
-unsigned long timer, upperTime;
+unsigned long timerPB, upperTime;
 
 // constantes, diferenciales, etc;
-double FREQ;
-unsigned long duration;
 float noteFrequencies[6]={82.4, 110, 146.8, 196, 246.9, 329.6}; //unidad en Hz
-short servoMovement=8; // que tanto se movera el servo
+short servoMovement=10; // que tanto se movera el servo
 float df=10; //margen de las frecuencias
+
+/*
+ * Empiezan variables para identificar la frecuencia
+ * CAT
+ */
+ 
+//data storage variables
+byte newData = 0;
+byte prevData = 0;
+unsigned int time = 0;//keeps time and sends vales to store in timer[] occasionally
+int timer[10];//storage for timing of events
+int slope[10];//storage for slope of events
+unsigned int totalTimer;//used to calculate period
+unsigned int period;//storage for period of wave
+byte index = 0;//current storage index
+float frequency;//storage for frequency calculations
+int maxSlope = 0;//used to calculate max slope as trigger point
+int newSlope;//storage for incoming slope data
+
+//variables for deciding whether you have a match
+byte noMatch = 0;//counts how many non-matches you've received to reset variables if it's been too long
+byte slopeTol = 3;//slope tolerance- adjust this if you need
+int timerTol = 10;//timer tolerance- adjust this if you need
+
+//variables for amp detection
+unsigned int ampTimer = 0;
+byte maxAmp = 0;
+byte checkMaxAmp;
+byte ampThreshold = 30;//raise if you have a very noisy signal
+
+//End CAT
+
 
 //Variables de estado
 short currentNote; //E=1, A=2,..E=6
@@ -25,11 +55,9 @@ boolean isBtnUp;
 
 void setup() {
   Serial.begin(9600);
-  
   servo.attach(pinServo); //Declaracion del Servo
-  pinMode(FREQREADER, INPUT);
   
-  //Declaracion de los Botones
+    //Declaracion de los Botones
   pinMode(PBON, INPUT_PULLUP);
   pinMode(PBOFF, INPUT_PULLUP);
   pinMode(PBSTRING, INPUT_PULLUP);
@@ -45,12 +73,104 @@ void setup() {
   
   pinMode(LEDON, OUTPUT);
   digitalWrite(LEDON, HIGH);
-  
-  isTunning=false;
+
+  isTunning=true;
   isBtnUp=true;//el medina no sabe como programar;
   currentNote=0;
-  timer=0;
+  timerPB=0;
   upperTime=10;
+  frequency=0;
+  //servo.write(90);
+  //servo.writeMicroseconds(50);
+  cli();//disable interrupts
+  
+  //set up continuous sampling of analog pin 0 at 38.5kHz
+ 
+  //clear ADCSRA and ADCSRB registers
+  ADCSRA = 0;
+  ADCSRB = 0;
+  
+  ADMUX |= (1 << REFS0); //set reference voltage
+  ADMUX |= (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
+  
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32=500kHz
+  ADCSRA |= (1 << ADATE); //enabble auto trigger
+  ADCSRA |= (1 << ADIE); //enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //start ADC measurements
+  
+  sei();//enable interrupts
+}
+
+ISR(ADC_vect) {//when new ADC value ready
+                  
+  prevData = newData;//store previous value
+  newData = ADCH;//get value from A0
+  if (prevData < 127 && newData >=127){//if increasing and crossing midpoint
+    newSlope = newData - prevData;//calculate slope
+    if (abs(newSlope-maxSlope)<slopeTol){//if slopes are ==
+      //record new data and reset time
+      slope[index] = newSlope;
+      timer[index] = time;
+      time = 0;
+      if (index == 0){//new max slope just reset
+        PORTB |= B00010000;//set pin 12 high
+        noMatch = 0;
+        index++;//increment index
+      }
+      else if (abs(timer[0]-timer[index])<timerTol && abs(slope[0]-newSlope)<slopeTol){//if timer duration and slopes match
+        //sum timer values
+        totalTimer = 0;
+        for (byte i=0;i<index;i++){
+          totalTimer+=timer[i];
+        }
+        period = totalTimer;//set period
+        //reset new zero index values to compare with
+        timer[0] = timer[index];
+        slope[0] = slope[index];
+        index = 1;//set index to 1
+        PORTB |= B00010000;//set pin 12 high
+        noMatch = 0;
+      }
+      else{//crossing midpoint but not match
+        index++;//increment index
+        if (index > 9){
+          reset();
+        }
+      }
+    }
+    else if (newSlope>maxSlope){//if new slope is much larger than max slope
+      maxSlope = newSlope;
+      time = 0;//reset clock
+      noMatch = 0;
+      index = 0;//reset index
+    }
+    else{//slope not steep enough
+      noMatch++;//increment no match counter
+      if (noMatch>9){
+        reset();
+      }
+    }
+  }
+  
+  time++;//increment timer at rate of 38.5kHz
+  
+  ampTimer++;//increment amplitude timer
+  if (abs(127-ADCH)>maxAmp){
+    maxAmp = abs(127-ADCH);
+  }
+  if (ampTimer==1000){
+    ampTimer = 0;
+    checkMaxAmp = maxAmp;
+    maxAmp = 0;
+  }
+  
+}
+
+void reset(){//clean out some variables
+  index = 0;//reset index
+  noMatch = 0;//reset match couner
+  maxSlope = 0;//reset slope
 }
 
 void resetAll(){
@@ -60,57 +180,51 @@ void resetAll(){
   digitalWrite(LEDON, LOW);
 } 
 
-void loop(void) {
+void loop(){
   //
   if(!isTunning){
     if(digitalRead(PBSTRING)==LOW){
-      if(isBtnUp && timer==0){
+      if(isBtnUp && timerPB==0){
         turnOnLed();
-        timer=1; 
-        Serial.print("Numero");
-        Serial.println(timer);
+        timerPB=1; 
+        //Serial.print("Numero");
+        //Serial.println(timerPB);
       }
       isBtnUp=false;
     }
      else if(digitalRead(PBOFF)==LOW){
-      if(isBtnUp && timer==0){
+      if(isBtnUp && timerPB==0){
         resetAll();
-        timer=1;
+        timerPB=1;
       }
       isBtnUp=false;
      }
      else if(digitalRead(PBSTART)==LOW){
-      if(isBtnUp && timer==0){
+      if(isBtnUp && timerPB==0){
         isTunning=true;
-        timer=1;
+        timerPB=1;
       }
       isBtnUp=false;
      }
      else
       isBtnUp=true;
   }
-  else {
-    duration = pulseIn(FREQREADER, HIGH);
-    if(duration!=0)
-      FREQ= 500000/duration;
-    else FREQ=0;
-    //checkFreq();
-    //moveServo();
-    Serial.print("Frecuencia.  ");
-    Serial.println(FREQ); 
+  else { 
+    if (checkMaxAmp>ampThreshold){
+      frequency = 38462/float(period);//calculate frequen      servo.detach();cy timer rate/period
+      Serial.print("Frecuencia: ");
+      Serial.println(frequency);
+      moveServo();
+    }
+    if(digitalRead(PBSTRING)==LOW)
+          servo.detach();
   }
-  if(timer!=0)
-     timer++;
-  if(timer==upperTime)
-    timer=0;
-  Serial.println(timer);
+  if(timerPB!=0)
+     timerPB++;
+  if(timerPB==upperTime)
+    timerPB=0;
+  //Serial.println(timer);
 }
-/*
-void checkFreq(void){
-  for(int c=0; c<6; c++)
-    if(noteFrequencies[c]-df <= FREQ && noteFrequencies[c]+df >= FREQ)
-      Serial.print(c);
-} */
 
 void turnOnLed(void){
   Serial.print("Nota actual: ");
@@ -132,6 +246,8 @@ void stringDone(void){
 
 void moveServo(void){
     Serial.println("Servo se va a mover");
+    servo.write(90+servoMovement);
+    /*
     if(noteFrequencies[currentNote-1]-df < FREQ-df){ //Mover clockWise  
       servo.write(90+servoMovement);// sets the servo position according to the scaled value
       delay(15); // waits for the servo to get there
@@ -142,4 +258,5 @@ void moveServo(void){
     }
     else
       stringDone();
+      */
 }
